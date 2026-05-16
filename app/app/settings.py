32d71aa7ac9640ba2ff1b20b11b3f4b90eb15fa7
Gayatri-Bhagwat <gayatribhagwat2043@gmail.com>
2026-05-16 16:38:52 +0530

    updated settings.py file to use postgres

--- app/app/settings.py
+++ app/app/settings.py
@@ -84,17 +84,13 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.environ.get('DB_NAME', 'devdb'),
-    #     'USER': os.environ.get('DB_USER', 'devuser'),
-    #     'PASSWORD': os.environ.get('DB_PASS', 'changeme'),
-    #     'HOST': os.environ.get('DB_HOST', 'db'),
-    #     'PORT': os.environ.get('DB_PORT', '5432'),
-    # }
-"default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
